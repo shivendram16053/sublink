9f548877c17be37decd4f58bfb9a8f8fcc065a0e
Shivendra Mishra <shivendramishra.sm93@gmail.com>
2024-08-28 16:55:50 +0530

    check added

--- src/app/api/actions/create/route.ts
+++ src/app/api/actions/create/route.ts
@@ -12,22 +12,24 @@ import {
   createPostResponse,
   ActionGetResponse,
   ActionPostResponse,
+  createActionHeaders,
 } from "@solana/actions";
 import { connectToDatabase } from "../../../(mongo)/db"; // adjust the path as necessary
-import OrgData from "../../../(mongo)/OrgSchema";
-import { customAlphabet } from "nanoid";
-import { getCompletedAction } from "../helper";
+import { customAlphabet, random } from "nanoid";
+import { getCompletedAction, saveOrgData } from "../helper";
+import OrgData from "@/app/(mongo)/OrgSchema";
 
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 const MY_PUB_KEY = "6rSrLGuhPEpxGqmbZzV1ZttwtLXzGx8V2WEACXd4qnVH";
 const generateRandomId = customAlphabet("abcdefghijklmnopqrstuvwxyz", 8);
+const headers = createActionHeaders();
 
-export const GET= async(req: NextRequest) =>{
-  
+export const GET = async (req: NextRequest) => {
   const payload: ActionGetResponse = {
     icon: new URL("/logo.png", new URL(req.url).origin).toString(),
     title: "Create your own subscription Blink",
-    description: "Enter the details of your organisation/business/project to create a blink",
+    description:
+      "Enter the details of your organisation/business/project to create a blink",
     label: "Create One",
     links: {
       actions: [
@@ -36,9 +38,24 @@ export const GET= async(req: NextRequest) =>{
           href: "/api/actions/create?name={name}&email={email}&month={month}&year={year}",
           parameters: [
             { type: "text", name: "name", label: "Enter Name", required: true },
-            { type: "email", name: "email", label: "Enter Email", required: true },
-            { type: "number", name: "month", label: "Enter Monthly Price", required: true },
-            { type: "number", name: "year", label: "Enter Yearly Price", required: true },
+            {
+              type: "email",
+              name: "email",
+              label: "Enter Email",
+              required: true,
+            },
+            {
+              type: "number",
+              name: "month",
+              label: "Enter Monthly Price",
+              required: true,
+            },
+            {
+              type: "number",
+              name: "year",
+              label: "Enter Yearly Price",
+              required: true,
+            },
           ],
         },
       ],
@@ -49,12 +66,12 @@ export const GET= async(req: NextRequest) =>{
   return NextResponse.json(payload, {
     headers: ACTIONS_CORS_HEADERS,
   });
-}
+};
 
 // ensures CORS
-export const OPTIONS = GET;
+export const OPTIONS = async () => Response.json(null, { headers });
 
-export const POST = async (req: NextRequest) =>{
+export const POST = async (req: NextRequest) => {
   try {
     await connectToDatabase();
 
@@ -65,7 +82,8 @@ export const POST = async (req: NextRequest) =>{
     const month = parseFloat(searchParams.get("month") ?? "0");
     const year = parseFloat(searchParams.get("year") ?? "0");
     const randomId = generateRandomId();
-    const orgPubKey = new PublicKey(body.account);
+    const orgKey = body.account;
+    const orgPubKey = new PublicKey(orgKey);
 
     const transaction = new Transaction().add(
       SystemProgram.transfer({
@@ -76,25 +94,22 @@ export const POST = async (req: NextRequest) =>{
     );
 
     transaction.feePayer = orgPubKey;
-    transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
 
     // Check if the transaction was already signed and completed
     if (body.signature) {
-
-      console.log(" is done")
-      // Save the data to MongoDB after transaction confirmation
       const newBlink = new OrgData({
-        name,
         org: randomId,
+        name,
         email,
         month,
         year,
-        orgPubKey: body.account,
+        orgPubKey,
       });
 
       await newBlink.save();
-
-      console.log("data saved")
 
       return NextResponse.json(
         await createPostResponse({
@@ -112,17 +127,16 @@ export const POST = async (req: NextRequest) =>{
       );
     }
 
-    console.log("trnasaction sgined")
-    // If not signed, return the transaction for the user to sign
+    console.log("transaction signed");
+
+    const nextActionLink = await saveOrgData(name, email, month, year, orgKey);
+
     const payload = await createPostResponse({
       fields: {
         transaction,
         message: "Please sign the transaction to complete the process.",
         links: {
-          next: {
-            type: "post",
-            href: `/api/actions/create?name=${name}&email=${email}&month=${month}&year=${year}&randomId=${randomId}`,
-          },
+          next: nextActionLink,
         },
       },
     });
@@ -140,4 +154,4 @@ export const POST = async (req: NextRequest) =>{
       }
     );
   }
-}
+};
--- src/app/api/actions/helper.ts
+++ src/app/api/actions/helper.ts
@@ -1,7 +1,7 @@
 import { NextActionLink } from "@solana/actions-spec";
 
 // just a helper function to get the completed action at final stage [ last action in the chain ]
-export const getCompletedAction = (orgid:string): NextActionLink => {
+export const getCompletedAction = (orgid: string): NextActionLink => {
   return {
     type: "inline",
     action: {
@@ -12,4 +12,42 @@ export const getCompletedAction = (orgid:string): NextActionLink => {
       type: "completed",
     },
   };
+};
+export const saveOrgData = (
+  name: string,
+  email: string,
+  month: number,
+  year: number,
+  orgKey: string
+): NextActionLink => {
+  try {
+    return {
+      type: "inline",
+      action: {
+        description: `Confirm the details and create you blink
+         Name : ${name}
+         Email : ${email}
+         Monthly fees : ${month}
+         Yearly Fees : ${year}
+         Public Key to recieve fees : ${orgKey}
+        `,
+
+        icon: `https://subslink.vercel.app/logo.png`,
+        label: `Confirm detail`,
+        title: `Confirmation`,
+        type: "action",
+        links: {
+          actions: [
+            {
+              label: `Confirm`, // button text
+              href: `/api/action/create`, // api endpoint
+            },
+          ],
+        },
+      },
+    };
+  } catch (error) {
+    console.error("Error saving organization data:", error);
+    throw new Error("Failed to save organization data.");
+  }
 };
