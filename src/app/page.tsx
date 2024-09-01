72d27efb966337f882c43f40f17b52a63105fe97
Shivendra Mishra <shivendramishra.sm93@gmail.com>
2024-09-01 14:11:22 +0530

    Landing page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,33 @@
-import Image from "next/image";
+import Link from 'next/link';
+import React from 'react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-900">
-      <header className="text-center mb-12">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Subslink</h1>
-        <p className="text-lg mb-6">
-          Simplify your subscription management with customizable Solana-based Blinks.
-        </p>
-          <div className="flex justify-center items-center">
-          <Image
-            src="/logo.png"
-            alt="Sublink Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          </div>
-      </header>
-
-      <button className="bg-black text-white p-3 mb-2 border-slate-700">
-        <a href="https://dial.to/?action=solana-action%3Ahttps%3A%2F%2Fsubslink.vercel.app%2Fapi%2Factions%2Fcreate&cluster=devnet">Create Blink for your Project</a>
-      </button>
-
-      <section className="text-center mb-12">
-        <h2 className="text-2xl font-semibold mb-4">Why Choose Subslink?</h2>
-        <p className="text-lg mb-6">
-          Subslink offers a seamless way for organizations to manage their subscriptions. Create your custom Blink to:
-        </p>
-        <ul className="list-disc list-inside mb-6">
-          <li>Effortlessly handle subscription payments.</li>
-          <li>Track transactions in real-time.</li>
-          <li>Customize payment options to fit your needs.</li>
-        </ul>
+    <main className="flex flex-col items-center justify-center h-screen mt-[-90px]  text-white">
+      {/* Hero Section */}
+      <section className="text-center px-6 flex flex-col justify-center items-center">
+        <h1 className="text-7xl mb-4   text-blue-100">
+        Empower direct subscription with
+        </h1>
+        <h1 className="text-7xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r h-20 from-violet-600 via-cyan-500 to-blue-500">
+        Subslink
+        </h1>
+        <p className="text-2xl mb-8 text-gray-400">Seamless. Shareable. On Solana.</p>
+        
+        {/* Buttons */}
+        <div className="flex space-x-4">
+          <Link href="https://x.com/subs_link">
+          <button className="px-6 py-3 bg-gradient-to-r from-red-600 via-orange-500 to-yellow-500 rounded-full text-lg">
+            Follow on Twitter
+          </button>
+          </Link>
+          <Link href="https://dial.to/devnet?action=solana-action:https://subslink.vercel.app/api/actions/create">
+          <button className="px-6 py-3 bg-gradient-to-r from-blue-600  to-cyan-500 rounded-full text-lg">
+            Create Subscription
+          </button>
+          </Link>
+        </div>
       </section>
-
-      <footer className="text-center mt-12">
-        <p className="text-sm">
-          &copy; 2024 Subslink. All rights reserved.
-        </p>
-      </footer>
     </main>
   );
 }
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link href="/">
+        <>
+          <Image src="/logo.png" width={80} height={80} alt="logo" />
+        </>
+      </Link>
+
+      <div className="nav-item">
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="https://dial.to/devnet?action=solana-action:https://subslink.vercel.app/api/actions/create">
+              <div className="text-blue-500 hover:underline">Create a New Event</div>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
